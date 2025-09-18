3cedd2a1ecc39227a78a9079ef93cadfef1781a8
pavel_chernous <goltshteyngottorp@gmail.com>
2025-09-18 19:38:01 +0300

    Update main

--- main.py
+++ main.py
@@ -1,30 +1,32 @@
+import asyncio
 from aiogram import Bot, Dispatcher, types
-from aiogram.utils import executor
 from Gear.config import config
 from payments.yookassa import YooKassa
 from Gear.db import db
-from models import User
+from Gear.models import User
+
 
 bot = Bot(token=config.BOT_TOKEN)
-dp = Dispatcher(bot)
+dp = Dispatcher()
 
-# Инициализация базы (создание таблиц)
+
+# Создаём таблицы
 db.connect()
 db.create_tables([User], safe=True)
 db.close()
 
-@dp.message_handler(commands=["start"])
+@dp.message(commands=["start"])
 async def start(msg: types.Message):
     await msg.answer("Привет! Напиши /pay 100 чтобы протестировать оплату.")
 
-@dp.message_handler(commands=["pay"])
+@dp.message(commands=["pay"])
 async def pay(msg: types.Message):
     try:
         amount = int(msg.get_args())
     except ValueError:
         return await msg.answer("Укажите сумму: /pay 100")
 
-    # создаем или получаем пользователя
+    # создаём или получаем пользователя
     db.connect()
     user, _ = User.get_or_create(id=msg.from_user.id)
     db.close()
@@ -33,5 +35,8 @@ async def pay(msg: types.Message):
     url = yk.create_payment(amount, msg.from_user.id, "Пополнение баланса")
     await msg.answer(f"Оплатить можно здесь:\n{url}")
 
+async def main():
+    await dp.start_polling(bot)
+
 if __name__ == "__main__":
-    executor.start_polling(dp, skip_updates=True)
+    asyncio.run(main())
